901d03b63212c34e6d87cb51508c4d0925225439
Miguel Perez <miguel250@gmail.com>
2012-04-23 21:13:32 -0400

    Add python recipe

---
+++ mlpz/recipes/python.rb
@@ -0,0 +1,37 @@
+include_recipe "python"
+include_recipe "python::pip"
+
+
+#create python folder
+directory "/home/vagrant/python" do
+  owner "vagrant"
+  group "vagrant"
+  mode "0755"
+  action :create
+end
+
+#create virtual virtualenv
+python_virtualenv "/home/vagrant/python" do
+  interpreter "python2.7"
+  owner "vagrant"
+  group "vagrant"
+  action :create
+end
+
+python_pip "gunicorn" do
+  virtualenv "/home/vagrant/python"
+  version "0.14.2"
+  action :install
+end
+
+python_pip "django" do
+  virtualenv "/home/vagrant/python"
+  version "1.4"
+  action :install
+end
+
+python_pip "django-mongodb-engine" do
+  virtualenv "/home/vagrant/python"
+  version "0.4.0"
+  action :install
+end
